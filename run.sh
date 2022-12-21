1ae7745c37628deafe61b98fff9064feef08b666
ddkozyreva <ddkozyreva@edu.hse.ru>
2022-12-22 02:03:33 +0300

    2

--- run.sh
+++ run.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
-#SBATCH --job-name=hw3
-#SBATCH --account=proj_1339        # Название проекта (курса)
-#SBATCH --ntasks=1                 # Количество MPI процессов
-#SBATCH --nodes=1                  # Требуемое кол-во узлов
-#SBATCH --gpus=0                   # Требуемое кол-во GPU
-#SBATCH --cpus-per-task=32         # Требуемое кол-во CPU
-#SBATCH --constraint="type_a|type_b|type_c|type_d"
-#SBATCH --time=2
-
+#SBATCH -J mpi_c
+#SBATCH --ntasks-per-node=1
+#SBATCH -N 1
+#SBATCH -A proj_1339
+#SBATCH --error=error.txt
+#SBATCH --output=output.txt
 
 mpicxx -O3 eigenvector.cpp -o main 
-echo 1000 | mpirun ./main  > output.txt
+echo 1000 | srun --mpi=pmix ./main
